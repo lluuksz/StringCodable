7921b453da869a603529645808e950ea5c981fe9
lluuksz30 <lluuksz30@gmail.com>
2024-05-12 08:13:29 +0200

    Add encoded and decoded functions

--- Sources/StringCodable/StringCodable.swift
+++ Sources/StringCodable/StringCodable.swift
@@ -1,2 +1,18 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+extension String {
+    
+    public func base64Encoded() -> String? {
+       data(using: .utf8)?.base64EncodedString()
+    }
+     
+    public func base64Decoded() -> String? {
+       guard let data = Data(base64Encoded: self) else { return nil }
+       
+        return String(data: data, encoding: .utf8)
+    }
+    
+}
--- Tests/StringCodableTests/StringCodableTests.swift
+++ Tests/StringCodableTests/StringCodableTests.swift
@@ -2,11 +2,27 @@ import XCTest
 @testable import StringCodable
 
 final class StringCodableTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    func testBase64Encoded() throws {
+        // Given
+        let sut = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        
+        // When
+        let result = sut.base64Encoded()
+        
+        // Then
+        XCTAssertEqual(result, "TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdCwgc2VkIGRvIGVpdXNtb2QgdGVtcG9yIGluY2lkaWR1bnQgdXQgbGFib3JlIGV0IGRvbG9yZSBtYWduYSBhbGlxdWEu")
     }
+    
+    func testBase64Decoded() throws {
+        // Given
+        let sut = "TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdCwgc2VkIGRvIGVpdXNtb2QgdGVtcG9yIGluY2lkaWR1bnQgdXQgbGFib3JlIGV0IGRvbG9yZSBtYWduYSBhbGlxdWEu"
+        
+        // When
+        let result = sut.base64Decoded()
+        
+        // Then
+        XCTAssertEqual(result, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+    }
+    
 }
